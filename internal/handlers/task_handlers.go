8e4a77a4e283b6841b6913b86c8ca32533ce6d3a
999Marv <marvinsiri123@gmail.com>
2024-10-08 16:52:33 -0400

    handlers created

--- internal/handlers/task_handlers.go
+++ internal/handlers/task_handlers.go
@@ -1,1 +1,27 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/999Marv/task-manager-go/internal/models"
+	"github.com/999Marv/task-manager-go/internal/repository"
+	"github.com/gin-gonic/gin"
+)
+
+var taskRepo = repository.NewTaskRepository()
+
+func CreateTask(c *gin.Context) {
+	var newTask models.Task
+
+	if err := c.ShouldBindBodyWithJSON(&newTask); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	createdTask := taskRepo.Create(newTask)
+	c.JSON(http.StatusCreated, createdTask)
+}
+
+func GetAllTasks(c *gin.Context) {
+	tasks := taskRepo.GetAll()
+	c.JSON(http.StatusOK, tasks)
+}
